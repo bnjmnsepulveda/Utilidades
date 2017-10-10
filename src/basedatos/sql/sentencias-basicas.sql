bc23e2945b0589e52ac7b1442e7214a62344b306
Roberto <bnjmnsepulveda@gmail.com>
2017-10-10 13:00:12 -0300

    nuevos recursos

--- src/basedatos/sql/sentencias-basicas.sql
+++ src/basedatos/sql/sentencias-basicas.sql
@@ -9,3 +9,9 @@ ADD CONSTRAINT my_fk
 FOREIGN KEY (my_field) 
 REFERENCES my_foreign_table(id) 
 ON DELETE CASCADE;
+
+CREATE TABLE orders (
+    order_id integer PRIMARY KEY,
+    product_no integer REFERENCES products (product_no),
+    quantity integer
+);
